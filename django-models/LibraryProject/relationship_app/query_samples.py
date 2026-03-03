e9c02302b24f00e61251b19c435af0633b3668aa
Samuel Giks <samuelgiks254@gmail.com>
2026-03-03 18:56:46 +0300

    filtering author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,7 +10,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # 1️⃣ Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = author.books.filter(authior=author)
     return books
 
 
